54bd8f38fe8fa86580dbb8fe25d83e849126576f
mdashnya <mdashnya@nbwin1255.campus.tue.nl>
2017-01-05 11:09:49 +0100

    -format change(bracket and indentaion) -added refresh function

--- src/gol/Cell.java
+++ src/gol/Cell.java
@@ -16,56 +16,51 @@ import javax.swing.JPanel;
  */
 class Cell extends JPanel {
 
-        private Color defaultBackground;
-        private boolean isAlive;
-        private int xPos;
-        private int yPos;
-        
-        public Cell(int xpos, int ypos, boolean isalive) {
-            xPos = xpos;
-            yPos = ypos;
-            isAlive = isalive;
-            if(isAlive)
-            {
-                defaultBackground = getBackground();
-                setBackground(Color.BLACK);
-            }
-        }
+    private Color defaultBackground;
+    private boolean isAlive;
+    private int xPosition;
+    private int yPosition;
 
-        @Override
-        public Dimension getPreferredSize() {
-            return new Dimension(15, 15);
+    public Cell(int xpos, int ypos, boolean isalive) {
+        xPosition = xpos;
+        yPosition = ypos;
+        isAlive = isalive;
+        if (isAlive) {
+            defaultBackground = getBackground();
+            setBackground(Color.BLACK);
         }
-        
-        public Cell(int xpos, int ypos) {
-            xPos = xpos;
-            yPos = ypos;
-        }
-        
-        @Override
-        public boolean equals(Object object)
-        {
-            boolean sameSame = false;
+    }
+    
+    public Cell(int xpos, int ypos) {
+        xPosition = xpos;
+        yPosition = ypos;
+    }
+    
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(15, 15);
+    }
 
-            if (object != null && object instanceof Cell)
-            {
-                sameSame = this.xPos == ((Cell) object).xPos && this.yPos == ((Cell) object).yPos;
-            }
-
-            return sameSame;
+    @Override
+    public boolean equals(Object object) {
+        boolean sameSame = false;
+        if (object != null && object instanceof Cell) {
+            sameSame = this.xPosition == ((Cell) object).xPosition && this.yPosition == ((Cell) object).yPosition;
         }
-        public ArrayList<Cell> getNeighbours()
-        {
-            ArrayList<Cell> neighbours=new ArrayList<>();
-            int xpos=this.xPos;
-            int ypos=this.yPos;
-            for (int i=xpos-1; i<=xpos+1; i++)
-                for(int j=ypos-1; j<=ypos+1; j++)
-                {
-                    if (j!=ypos || i!=xpos) {
-                        neighbours.add(new Cell(i,j));
-                    }
+        return sameSame;
+    }
+
+    public ArrayList<Cell> getNeighbours() {
+        ArrayList<Cell> neighbours = new ArrayList<>();
+        int xpos = this.xPosition;
+        int ypos = this.yPosition;
+        for (int i = xpos - 1; i <= xpos + 1; i++) {
+            for (int j = ypos - 1; j <= ypos + 1; j++) {
+                if (j != ypos || i != xpos) {
+                    neighbours.add(new Cell(i, j));
                 }
-            return neighbours;
+            }
         }
+        return neighbours;
+    }
 }
